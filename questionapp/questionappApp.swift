231b2f1a738050153137e3260d00f3d1464b1312
Inaya Dar <inayadar@gmail.com>
2024-06-24 10:06:27 -0500

    Initial Commit

---
+++ questionapp/questionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionappApp.swift
+//  questionapp
+//
+//  Created by Inaya Dar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct questionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
